801660f0bfcb6254e0fd9d4ba189a05a73c60332
Bruno D'Luka <brunodlukaa@gmail.com>
2024-12-10 15:09:43 -0300

    feat: Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
+import Header from "../../components/Header/Header";
+
+import "./Home.css";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>This is the home page of our application.</p>
+        <div className="container">
+          <Header />
+    
+          <div className="content">
+            <h1 className="title">CYBERNETICA</h1>
+            <p className="description">
+              Associação Atlética Acadêmica De Ciência da Computação, Análise e Desenvolvimento de Sistemas e Ciência de Dados do CEUB
+            </p>
+          </div>
         </div>
-    );
+      );
 };
 
 export default Home;
