d123283778144418176d3abd545203a38b78282f
Bruno D'Luka <brunodlukaa@gmail.com>
2024-12-17 02:22:38 -0300

    feat: Home/News

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 
+import NewsSection from "./sections/News";
+
 import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className="container">
-          <Header />
-    
-          <div className="content">
-            <h1 className="title">CYBERNETICA</h1>
-            <p className="description">
-              Associação Atlética Acadêmica De Ciência da Computação, Análise e Desenvolvimento de Sistemas e Ciência de Dados do CEUB
-            </p>
-          </div>
+  return (
+    <div className="home">
+      <div className="container" scroll>
+        <Header />
+
+        <div className="content">
+          <h1 className="title">CYBERNETICA</h1>
+          <p className="description">
+            Associação Atlética Acadêmica De Ciência da Computação, Análise e
+            Desenvolvimento de Sistemas e Ciência de Dados do CEUB
+          </p>
         </div>
-      );
+      </div>
+
+      <NewsSection />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/Home/sections/News.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./News.css";
+
+const NewsSection = () => {
+    return (
+        <div className="news">
+            <h1 className="title">Novidades</h1>
+
+            <div className="news-list">
+                <img src="https://placehold.co/200" alt="News 1" className="news-item" />
+                <div className="spacing" /> {/* there is probably a better way to do this spacing */}
+                <img src="https://placehold.co/200" alt="News 2" className="news-item" />
+                <div className="spacing" /> {/* there is probably a better way to do this spacing */}
+                <img src="https://placehold.co/200" alt="News 3" className="news-item" />
+            </div>
+
+            <h3 className="see-more">Ver mais</h3>
+        </div>
+    );
+}
+
+export default NewsSection;
