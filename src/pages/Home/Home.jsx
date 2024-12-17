84173d63329bc20efe0e38335983f39c477c3cfc
Bruno D'Luka <brunodlukaa@gmail.com>
2024-12-17 02:23:05 -0300

    chore: Imports

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 
-import NewsSection from "./sections/News";
+import NewsSection from "./News/News";
 
 import "./Home.css";
 
