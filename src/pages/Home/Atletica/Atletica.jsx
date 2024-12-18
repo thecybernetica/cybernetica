583afffe4783e3c83566f1369f76d45e4a01686d
Bruno D'Luka <brunodlukaa@gmail.com>
2024-12-18 12:10:16 -0300

    feat: Atletilca card title

--- src/pages/Home/Atletica/Atletica.jsx
+++ src/pages/Home/Atletica/Atletica.jsx
@@ -3,29 +3,108 @@ import React from "react";
 import "./Atletica.css";
 
 const Atletica = () => {
-  return <div className="atletica">
-    <h1>A atlética</h1>
-    <div className="image-row">
-        <img src="https://placehold.co/200" alt="Presidente" className="image-card" />
-        <img src="https://placehold.co/200" alt="Vice-Presidente" className="image-card" />
-        <div className="image-card" />
-        <img src="https://placehold.co/200" alt="Jurídico" className="image-card" />
-    </div>
-    <div className="image-row">
-        <img src="https://placehold.co/200" alt="Presidente" className="image-card" />
-        <img src="https://placehold.co/200" alt="Vice-Presidente" className="image-card" />
-        <img src="https://placehold.co/200" alt="Vice-Presidente" className="image-card" />
-        <img src="https://placehold.co/200" alt="Jurídico" className="image-card" />
-    </div>
-    <div className="image-row">
-        <img src="https://placehold.co/200" alt="Presidente" className="image-card" />
-        <img src="https://placehold.co/200" alt="Vice-Presidente" className="image-card" />
-        <div className="image-card" />
-        <div className="image-card" />
-    </div>
+  return (
+    <div className="atletica">
+      <h1>A atlética</h1>
+      <div className="image-row">
+        <div className="card">
+          <img
+            src="https://placehold.co/200"
+            alt="Presidente"
+            className="card-image"
+          />
+          <h3 className="card-title">Presidente</h3>
+        </div>
+        <div className="card">
+          <img
+            src="https://placehold.co/200"
+            alt="Vice-Presidente"
+            className="card-image"
+          />
+          <h3 className="card-title">Vice-Presidente</h3>
+        </div>
+        <div className="card" />
+        <div className="card">
+          <img
+            src="https://placehold.co/200"
+            alt="Jurídico"
+            className="card-image"
+          />
+          <h3 className="card-title">Jurídico</h3>
+        </div>
+      </div>
+      <div className="image-row">
+        <div className="card">
+          <img
+            src="https://placehold.co/200"
+            alt="Diretor de Produtos"
+            className="card-image"
+          />
+          <h3 className="card-title">D. Produtos</h3>
+        </div>
+        <div className="card">
+          <img
+            src="https://placehold.co/200"
+            alt="Diretor de e-sports"
+            className="card-image"
+          />
+          <h3 className="card-title">D. E-Sports</h3>
+        </div>
+        <div className="card">
+          <img
+            src="https://placehold.co/200"
+            alt="Diretor de E-Sports"
+            className="card-image"
+          />
+          <h3 className="card-title">D. E-Sports</h3>
+        </div>
+        <div className="card">
+          <img
+            src="https://placehold.co/200"
+            alt="Diretor de Eventos"
+            className="card-image"
+          />
+          <h3 className="card-title">D. Eventos</h3>
+        </div>
+      </div>
+      <div className="image-row">
+        <div className="card">
+          <img
+            src="https://placehold.co/200"
+            alt="Diretor de Marketing"
+            className="card-image"
+          />
+          <h3 className="card-title">D. Marketing</h3>
+        </div>
+        <div className="card">
+          <img
+            src="https://placehold.co/200"
+            alt="Diretor de Esportes"
+            className="card-image"
+          />
+          <h3 className="card-title">D. Esportes</h3>
+        </div>
+        <div className="card">
+          {/* <img
+            src="https://placehold.co/200"
+            alt="Jurídico"
+            className="card-image"
+          />
+          <h3 className="card-title">Jurídico</h3> */}
+        </div>
+        <div className="card">
+          {/* <img
+            src="https://placehold.co/200"
+            alt="Jurídico"
+            className="card-image"
+          />
+          <h3 className="card-title">Jurídico</h3> */}
+        </div>
+      </div>
 
-    <h3 className="see-more">Ver mais</h3>
-  </div>;
+      <h3 className="see-more">Ver mais</h3>
+    </div>
+  );
 };
 
 export default Atletica;
