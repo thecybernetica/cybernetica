b57bb9d68eaa7cd0bc84c7899ade49dd729e1217
Bruno D'Luka <brunodlukaa@gmail.com>
2024-12-20 02:37:05 -0300

    feat: Sports page

--- src/pages/Sports/Sports.jsx
+++ src/pages/Sports/Sports.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import "./Sports.css";
+import "../../styles/AppStyle.css";
 
 const Sports = () => {
   return (
@@ -8,9 +9,47 @@ const Sports = () => {
       <div className="container">
         <Header />
 
-        <div className="content">
-          <h1 className="title">FAÇA PARTE</h1>
-          <p className="description">e compita junto à Cybernética</p>
+        <div className="image-container">
+          <div className="content">
+            <h1 className="title">FAÇA PARTE</h1>
+            <p className="description">e compita junto à Cybernética</p>
+          </div>
+
+          <div className="image" />
+        </div>
+      </div>
+
+      <div className="secondary-container">
+        <div>
+          <div>
+            <h3>Futsal</h3>
+            <p> ={">"} Quinta-Feira, às 14h até 17h</p>
+            <p> ={">"} CEUB Asa Norte Bloco 04</p>
+          </div>
+          <div>
+            <h3>Vôlei</h3>
+            <p> ={">"} Terça-Feira, às 14h até 17h</p>
+            <p> ={">"} CEUB Asa Norte Bloco 04</p>
+          </div>
+        </div>
+
+        <div
+          style={{
+            marginRight: "50px",
+          }}
+        />
+
+        <div>
+          <div>
+            <h3>Basquete</h3>
+            <p> ={">"} Sexta-Feira, às 16h até 18h</p>
+            <p> ={">"} CEUB Asa Norte Bloco 04</p>
+          </div>
+          <div>
+            <h3>Handebol</h3>
+            <p> ={">"} Segunda-Feira, às 16h até 17h</p>
+            <p> ={">"} CEUB Asa Norte Bloco 04</p>
+          </div>
         </div>
       </div>
     </div>
