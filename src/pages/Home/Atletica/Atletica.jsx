4d59477e5d9f41422611b01d7aced22ed61f26a6
Bruno D'Luka <brunodlukaa@gmail.com>
2024-12-27 20:32:41 -0300

    feat: Standard "see more" button

--- src/pages/Home/Atletica/Atletica.jsx
+++ src/pages/Home/Atletica/Atletica.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ImageCard from "../../../components/ImageCard/ImageCard";
 
 import "./Atletica.css";
+import "../../../styles/AppStyle.css"
 
 const Atletica = () => {
   return (
--- src/pages/Home/News/News.jsx
+++ src/pages/Home/News/News.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 
 import "./News.css";
+import "../../../styles/AppStyle.css";
 
 const NewsSection = () => {
-    return (
-        <div className="news">
-            <h1 className="title">Blog da Cyber</h1>
+  return (
+    <div className="news">
+      <h1 className="title">Blog da Cyber</h1>
 
-            <div className="news-list">
-                <img src="https://placehold.co/200" alt="News 1" className="news-item" />
-                <div className="spacing" /> {/* there is probably a better way to do this spacing */}
-                <img src="https://placehold.co/200" alt="News 2" className="news-item" />
-                <div className="spacing" /> {/* there is probably a better way to do this spacing */}
-                <img src="https://placehold.co/200" alt="News 3" className="news-item" />
-            </div>
+      <div className="news-list">
+        <img
+          src="https://placehold.co/200"
+          alt="News 1"
+          className="news-item"
+        />
+        <div className="spacing" />{" "}
+        {/* there is probably a better way to do this spacing */}
+        <img
+          src="https://placehold.co/200"
+          alt="News 2"
+          className="news-item"
+        />
+        <div className="spacing" />{" "}
+        {/* there is probably a better way to do this spacing */}
+        <img
+          src="https://placehold.co/200"
+          alt="News 3"
+          className="news-item"
+        />
+      </div>
 
-            <h3 className="see-more">Ver mais</h3>
-        </div>
-    );
-}
+      <a href="/atletica" className="see-more">
+        Ver mais
+      </a>
+    </div>
+  );
+};
 
 export default NewsSection;
